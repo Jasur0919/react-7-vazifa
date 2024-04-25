4f43d9db25d88bc4e825e7de0686a4b6c63a4099
Jasur0919 <jasurjohn1111@gmail.com>
2024-04-25 11:17:39 +0500

    map

--- src/pages/single-rout/SingleRout.jsx
+++ src/pages/single-rout/SingleRout.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
+import './SingleRout.css'
 import React, {useEffect, useState} from "react";
 import { useParams } from "react-router-dom"
-import Image from '../../assets/blog-home.svg'
+// import Image from '../../assets/blog-home.svg'
 
 
 const SingleRout = () => {
@@ -18,26 +19,49 @@ const [path, setPath] = useState(null)
     console.log(path);
     
     
-   
+//    let link = path?.slice(0, 3).map((element, index) => (
+//     <div key={index} className="single_image_appearances">
+//                    <img  src={element.images} alt="" />
+//                    <img  src={element.images} alt="" />
+//                    <img  src={element.images} alt="" />
+//     </div>
+//    ))
 
   return (
+    
     <div>
+        
         <div className="container">
-            {/* <div className="single_cards">
-                <div className="single_image">
-                    <img src={Image} alt="" />
-                </div>
-            </div> */}
+            <div className="single_cards">
 
-{
-        path?.images?.map((e, inx) => (
-            <div key={inx} className="l">
-                <img  src={e} alt="" />   
-            </div>
-        ))
-    }
+                <div className="single_image_appearances">
+
+                    {
+                        path?.images?.slice(0, 3).map((element, index) => (
+                            <div key={index} className="ll">
+                                        <img  src={element} />
+                            </div>
+                        ))
+                    }
+                </div>
+
+                {/* {link} */}
+                {/* <div className="single_image_appearances">
+                 
+                </div> */}
+
+                <div className="single_image">
+                  
+                         <img  src={path.images[0]} alt="" />   
+                     </div>
+
+            </div> 
+
+
+
         </div>
         
+
 
         
        
